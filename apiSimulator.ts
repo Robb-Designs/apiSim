29fae52e5438c7c1170246467487f94412bac676
Robb-Designs <rtlee@aggies.ncat.edu>
2026-04-06 09:23:05 -0400

    made the logic for fetching product reviews

--- apiSimulator.ts
+++ apiSimulator.ts
@@ -1,12 +1,15 @@
 
+type Review = {
+  rating: number;
+  comment: string;
+};
 
 export const fetchProductCatalog = (): Promise<
     {
         id: number;
         name: string;
         price: number
-    }[]
-> => {
+    }[] > => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (Math.random() < 0.8) {
@@ -20,3 +23,20 @@ export const fetchProductCatalog = (): Promise<
         }, 1000);
     });
 };
+
+
+export const fetchProductReviews = (productId: number): Promise<Review[]> => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() < 0.8) {
+        resolve([
+          { rating: 5, comment: "Amazing" },
+          { rating: 4, comment: "Pretty nice product" },
+        ]);
+      } else {
+        reject(`Failed to fetch reviews for product ID ${productId}`);
+      }
+    }, 1500);
+  });
+};
+
